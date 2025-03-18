155c3f97b85cb8d0c2bbc0cf114d9138f58900fb
shrayosee-chowdhury <shrayosee2002@gmail.com>
2025-03-18 20:22:45 +0530

    Add files via upload

---
+++ largestElement.js
@@ -0,0 +1,5 @@
+function largest (arr){
+    return Math.max (...arr);
+}
+const arr =[10,1000,700,60,890];
+console.log(largest(arr));
---
+++ smallestElement.js
@@ -0,0 +1,5 @@
+function smallest (arr){
+    return Math.min (...arr);
+}
+const arr =[10, 90, 1, 3, 789];
+console.log(smallest(arr));
